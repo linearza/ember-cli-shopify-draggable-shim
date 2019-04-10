fdba89be1c03062be30b64cdfb96427c00957a2e
Michiel <linearza@users.noreply.github.com>
2019-04-10 07:20:31 +0200

    Fastboot compatibility

--- index.js
+++ index.js
@@ -4,22 +4,25 @@
 var path = require('path');
 var Funnel = require('broccoli-funnel');
 var MergeTrees = require('broccoli-merge-trees');
+var map = require('broccoli-stew').map;
 
 module.exports = {
   name: 'ember-cli-shopify-draggable',
+
+  treeForVendor(vendorTree) {
+    var draggablePath = path.dirname(require.resolve('@shopify/draggable/lib/draggable.bundle.js'));
+    var draggableTree = new Funnel(draggablePath, {
+      files: ['draggable.bundle.js']
+    });
+
+    draggableTree = map(draggableTree, (content) => `if (typeof FastBoot === 'undefined') { ${content} }`);
+
+    return new MergeTrees([vendorTree, draggableTree]);
+  },
 
   included() {
     this._super.included.apply(this, arguments);
     this.import('vendor/draggable.bundle.js');
     this.import('vendor/shims/shopify-draggable.js');
-  },
-
-  treeForVendor(vendorTree) {
-		var draggablePath = path.dirname(require.resolve('@shopify/draggable/lib/draggable.bundle.js'));
-		var draggableTree = new Funnel(draggablePath, {
-      files: ['draggable.bundle.js']
-    });
-
-    return new MergeTrees([vendorTree, draggableTree]);
-  },
+  }
 };
